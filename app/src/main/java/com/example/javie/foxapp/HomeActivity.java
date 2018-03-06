1621d65c67d2a74619fc802da7b104c3a761cbaf
Javier <javieerperez@hotmail.com>
2018-03-06 18:37:22 +0100

    commit github

--- app/src/main/java/com/example/javie/foxapp/HomeActivity.java
+++ app/src/main/java/com/example/javie/foxapp/HomeActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 public class HomeActivity extends AppCompatActivity {
 
 
-    SwipeRefreshLayout srl_tablon_refrescar;
+    SwipeRefreshLayout mSwRellay;
     RelativeLayout RltContenedor;
 
     @Override
@@ -27,32 +27,22 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        srl_tablon_refrescar = (SwipeRefreshLayout) findViewById(R.id.srl_tablon_refrescar);
-        srl_tablon_refrescar.setOnRefreshListener(refrescarTablon);
+        mSwRellay = (SwipeRefreshLayout) findViewById(R.id.swRleLay);
+        mSwRellay.setOnRefreshListener(refrescarVista);
         RltContenedor = findViewById(R.id.mRlLy);
     }
 
-    protected SwipeRefreshLayout.OnRefreshListener refrescarTablon = new SwipeRefreshLayout.OnRefreshListener() {
+    protected SwipeRefreshLayout.OnRefreshListener refrescarVista = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
             Snackbar.make(RltContenedor,"Lost your FOX?",Snackbar.LENGTH_LONG).show();
-            srl_tablon_refrescar.setRefreshing(false);
+            mSwRellay.setRefreshing(false);
         }
     };
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.menu_arriba,menu);
         return true;
-    }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.opciones) {
-            Toast.makeText(this, "Seleccionada opciones", Toast.LENGTH_LONG).show();
-        } else if (id == R.id.papelera) {
-            Toast.makeText(this, "Seleccionada cámara", Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
